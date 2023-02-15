dcab97229a57e14895d07d2305a37995f2962180
0uizi0 <0uizi0@naver.com>
2023-02-15 15:32:30 +0900

    HEADER search js 추가(풀이코드)

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// MAIN HEADER
+// SEARCH
+const searchEl = document.querySelector(".search");
+const searchInputEl = searchEl.querySelector("input");
+const searchIconEl = searchEl.querySelector("span");
+
+/**
+ * 깔끔한 코드
+ */
+let isFocused = false;
+
+searchIconEl.addEventListener("click", function () {
+  if (isFocused) {
+    searchInputEl.blur();
+    isFocused = false;
+  } else {
+    searchInputEl.focus();
+    isFocused = true;
+  }
+});
+
+searchInputEl.addEventListener("focus", function () {
+  searchInputEl.setAttribute("placeholder", "통합검색");
+});
+
+searchInputEl.addEventListener("blur", function () {
+  searchInputEl.setAttribute("placeholder", "");
+});
+
+/** 내 코드
+ * 1번째 시도 : width 값을 부여 => 36px인지 아닌지로 ==> 잘못된 코드.
+ * focus시 클래스 부여, click & blur시 클래스를 잃어버리게
+ */
