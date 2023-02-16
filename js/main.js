f2c35190ef689a6306970d34287b4ef5b337fc3c
0uizi0 <0uizi0@naver.com>
2023-02-16 14:26:15 +0900

    NOTICE promotion html,css,js 추가

--- js/main.js
+++ js/main.js
@@ -42,3 +42,47 @@ const swiperNotice = new Swiper(
     autoplay: true,
   }
 );
+// SWIPER PROMOTION
+const swiperPromotion = new Swiper(".notice .promotion .swiper", {
+  direction: "horizontal", //defalt값
+  slidesPerView: 3,
+  spaceBetween: 10,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".promotion .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    prevEl: ".promotion .swiper-button-prev",
+    nextEl: ".promotion .swiper-button-next",
+  },
+});
+
+// AutoPlay 컨트롤
+function controlAutoPlay() {
+  // console.log(swiperPromotion);
+  if (swiperPromotion.autoplay.running === false) {
+    swiperPromotion.autoplay.start();
+  } else {
+    swiperPromotion.autoplay.stop();
+  }
+}
+
+// Toggle Promotion
+const promotionSection = document.querySelector(".promotion");
+const promotionToggleBtn = document.querySelector(".notice .toggle-promotion");
+
+promotionToggleBtn.addEventListener("click", function () {
+  if (promotionSection.classList.contains("hide")) {
+    promotionSection.classList.remove("hide");
+    promotionToggleBtn.classList.add("show");
+  } else {
+    promotionSection.classList.add("hide");
+    promotionToggleBtn.classList.remove("show");
+  }
+});
